577dc86697cf4d2f2348669b09fd3a2738d7e53d
SAKURA-SAN23 <126578421+SAKURA-SAN23@users.noreply.github.com>
2023-03-01 11:54:13 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,7 @@
+document.getElementById('ver').onclick=function(){
+    document.getElementById('ver_mas').innerHTML = "Realizado es la Escuela  de Educacion Técnica N° 460 Guillermo Lehmann"
+}
+
+document.getElementById('ver_menos').onclick=function(){
+    document.getElementById('ver_mas').innerHTML=""
+}
